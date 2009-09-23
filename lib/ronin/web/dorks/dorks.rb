06db148ae6e3cc683d38e947a6de0005ed1447c2
postmodern <postmodern.mod3@gmail.com>
2009-09-23 16:11:03 -0700

    Don't require 'ronin/web/web'.

--- lib/ronin/web/dorks/dorks.rb
+++ lib/ronin/web/dorks/dorks.rb
@@ -19,9 +19,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/web/web'
-require 'ronin/extensions/uri'
-
 require 'gscraper/search'
 require 'digest/md5'
 
